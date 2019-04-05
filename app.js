425b83d0256463c742f94b3fb35128c1429d6af2
Matthew Cline <clinemattw@gmail.com>
2019-04-04 22:48:08 -0500

    add docker configuration

--- app.js
+++ app.js
@@ -8,18 +8,16 @@ var PORT = process.env.PORT || 3000;
 
 app.set("view engine", "ejs");
 
-// if(process.env.NODE_ENV === 'production') {
-app.use((req, res, next) => {
-    if (req.header('x-forwarded-proto') !== 'https') {
-        console.log("Redirected!!");
-        console.log(req.header);
-        res.redirect(`https://${req.header('host')}${req.url}`);
-    }
-    else {
-        next();
-    }
-});
-//   }
+if(process.env.ENV && process.env.ENV === 'production') {
+    app.use((req, res, next) => {
+        if (req.header('x-forwarded-proto') !== 'https') {
+            res.redirect(`https://${req.header('host')}${req.url}`);
+        }
+        else {
+            next();
+        }
+    });
+}
 
 app.get("/", (req, res) => {
     res.render("home");
