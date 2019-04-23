1c3c9bdd319a8bf10c6c5569ec3b379f20fcf503
Matthew Cline <clinemattw@gmail.com>
2019-04-23 07:58:21 -0700

    move front end code to client/

--- client/src/index.js
+++ client/src/index.js
@@ -3,5 +3,5 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('add_new'));
+ReactDOM.render(<App />, document.getElementById('root'));
 
