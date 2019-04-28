11bd754ff2947ffffc4fe8f1e611455156227e08
Matthew Cline <clinemattw@gmail.com>
2019-04-27 20:07:15 -0700

    play around with background colors

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -7,19 +7,21 @@ import './About.css';
 
 const About = () => {
   return (
-    <Container className="about-container text-center">
-      <h1>Go Share</h1>
-      <h1>
-        <Image 
-          src="https://raw.githubusercontent.com/matthewcline/gosharegood/master/client/public/imgs/good.png" 
-          fluid 
-        />
-      </h1>
-      <h5 className="mt-5">Looking for positive news report or feel-good stories?  On Go Share Good, members report
-        the good things that are happening in their world.
-      </h5>
-      <h4 className="mt-4"><a href="/signup">Sign up</a> to start sharing good!</h4>
-    </Container>
+    <div className="about-container">
+      <Container className="about-container-text text-center">
+        <h1>Go Share</h1>
+        <h1>
+          <Image 
+            src="https://raw.githubusercontent.com/matthewcline/gosharegood/master/client/public/imgs/good.png" 
+            fluid 
+          />
+        </h1>
+        <h5 className="mt-5">Looking for positive news report or feel-good stories?  On Go Share Good, members report
+          the good things that are happening in their world.
+        </h5>
+        <h4 className="mt-4"><a href="/signup">Sign up</a> to start sharing good!</h4>
+      </Container>
+    </div>
   );
 }
 
