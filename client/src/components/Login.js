4bda47581eb4f6f51cfbbeb47301080a34a30482
Matthew Cline <clinemattw@gmail.com>
2019-04-27 18:56:25 -0700

    add login circle

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -10,17 +10,17 @@ const Login = () => {
       <h1 className="text-center" style={{marginTop: '100px'}}>Login</h1>
       <Form action="http://localhost:3001/login" method="POST">
         <Form.Group controlId="formBasicUsername">
-          <Form.Label>Username</Form.Label>
-          <Form.Control type="text" name="username" placeholder="username" required/>
+          {/* <Form.Label>Username</Form.Label> */}
+          <Form.Control type="text" name="username" placeholder="Username" required/>
         </Form.Group>
 
         <Form.Group controlId="formBasicPassword">
-          <Form.Label>Password</Form.Label>
-          <Form.Control type="password" name="password" placeholder="password" />
+          {/* <Form.Label>Password</Form.Label> */}
+          <Form.Control type="password" name="password" placeholder="Password" />
         </Form.Group>
         
         <Button variant="primary" type="submit">
-          Submit
+          Login
         </Button>
       </Form>
     </Container>
