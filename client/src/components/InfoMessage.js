be2ab04030a664ae3b74feb5fd7ed110923f68ab
Matthew Cline <clinemattw@gmail.com>
2019-05-06 05:18:43 -0700

    add styling to InfoMessage

--- client/src/components/InfoMessage.js
+++ client/src/components/InfoMessage.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const InfoMessage = ({ type, text }) => {
   return (
-    <h5>
+    <h5 style={{marginBottom: '20px', backgroundColor: 'rgba(0,0,0,0.1'}}>
       { text }
     </h5>
   );
